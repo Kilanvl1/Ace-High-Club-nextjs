308023c95d83fcd6f263aee074022b831750cd00
Kilan_vanloo <Kilanvanloo@gmail.com>
2024-03-30 19:16:45 +0100

    feat: way too many commits

--- app/components/GroupsContainer.tsx
+++ app/components/GroupsContainer.tsx
@@ -1,8 +1,24 @@
+"use client";
+import { useState, useEffect } from "react";
 import { GroupPresentation } from "./GroupPresentation";
+import axios from "axios";
 export const GroupsContainer = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/hello-world/")
+      .then((response) => {
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="py-6 px-20">
       <GroupPresentation groupName="Pokaah NL" />
+      {message}
     </div>
   );
 };
